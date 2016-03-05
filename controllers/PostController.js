77abedc9c081770c057b33121c513efec2f13445
hoc <xuanhocbk@gmail.com>
2016-03-05 15:51:32 +0900

    Add create post view

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -6,4 +6,9 @@ router.get('/news', function(req, res, next) {
   res.render('Posts/news', { title: 'HackerNews', showNavbar: 1 });
 });
 
+
+/* submit a post */
+router.get('/submit', function(req, res, next) {
+  res.render('Posts/submit', { title: 'Create New', showNavbar: 1 });
+});
 module.exports = router;
