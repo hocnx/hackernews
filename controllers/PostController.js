c8bfb294d96c97cb6f7d537d0302edcf0e4f177b
hoc <xuanhocbk@gmail.com>
2016-03-05 14:17:50 +0900

    init project

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/news', function(req, res, next) {
+  res.render('Posts/news', { title: 'HackerNews', showNavbar: 1 });
 });
 
 module.exports = router;
