5c61bd694585d133f9a727bd61c1a937eb5e83d6
hoc <xuanhocbk@gmail.com>
2016-03-06 17:05:19 +0900

    fix some bug

--- models/comments/index.js
+++ models/comments/index.js
@@ -3,7 +3,6 @@ var Post = require('../posts/model');
 
 
 var DateHelper = require('../../helper/DateHelper');
-var Helper = require('../../helper/Helper');
 
 
 module.exports = {
@@ -15,9 +14,13 @@ module.exports = {
         comments.forEach(function(comment) {
           comment.timeTillNow  = DateHelper.getTimeTillNow(comment.created_at);
         });
-        callback(comments);
+        // find post
+        Post.findById(post_id, function(err, post){
+          post.timeTillNow = DateHelper.getTimeTillNow(post.created_at);
+          callback(post, comments);
+        });
       } else {
-        console.err(err);
+        console.error(err);
       }
     });
   },
@@ -35,7 +38,7 @@ module.exports = {
           }
         });
       } else {
-        console.err(err);
+        console.error(err);
       }
     });
   },
@@ -43,17 +46,18 @@ module.exports = {
   childComments: function(id, callback){
     Comment.find({parent_id: id},function(err, comments, count) {
       if(!err) {
+
         comments.forEach(function(comment) {
           comment.timeTillNow  = DateHelper.getTimeTillNow(comment.created_at);
         });
         callback(comments);
       } else {
-        console.err(err);
+        console.error(err);
       }
     });
   },
 
-  // add new comment
+  // add new comment for a Post
   addComment: function(postId, text, created_by, callback){
     Post.findById(postId, function(err, post) {
       console.log(post);
@@ -71,23 +75,58 @@ module.exports = {
           if(!err) {
             callback();
           } else {
-            console.err(err);
+            console.error(err);
           }
         });
       }else{
-        console.err(err);
+        console.error(err);
+      }
+    });
+  },
+
+  // add child comment
+  addChildComment: function(id, text, created_by, callback){
+    Comment.findById(id, function(err, comment) {
+
+      if (!err) {
+        Post.findById(comment.post_id, function(err, post){
+
+          post.num_of_comments += 1;
+          post.save();
+
+          new Comment({
+            text:text,
+            created_by:created_by,
+            title: post.title,
+            created_at: Date.now(),
+            post_id: post.id,
+            parent_id: comment.id
+
+          }).save(function(err, comment){
+            if(!err) {
+              callback();
+            } else {
+              console.error(err);
+            }
+          });
+        })
+
+      }else{
+        console.error(err);
       }
     });
   },
 
   findCommentById: function(id, callback){
     Comment.findById(id, function(err, comment) {
+
       if(!err){
         comment.timeTillNow = DateHelper.getTimeTillNow(comment.created_at);
         callback(comment);
       } else {
-        console.err(err);
+        console.error(err);
       }
+
     });
   }
 };
